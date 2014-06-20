43ab4e0adaabe3f4d43e7de52dbb82769fe515b1
Ken C <c.kennchin@gmail.com>
2014-06-20 15:31:55 -0700

    added rename_file function

---
+++ Exercise/Ex2/rename_file.py
@@ -0,0 +1,10 @@
+import os
+
+def rename_file():
+    #get file from folder
+    file_list = os.listdir(r"/Users/Ken/Desktop/Udacity_Programming_Python/Exercise/Ex2/prank")
+    print file_list
+    #rename file
+    new_list = os.rename(
+
+rename_file()
